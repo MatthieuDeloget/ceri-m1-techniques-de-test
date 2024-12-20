c07de3b703729ef968f2db21b7072617174ac05f
LaSauceBolo <matthieu.deloget@gmail.com>
2024-12-20 15:21:54 +0100

    TP4: Implémentation de PokemonFactory

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -0,0 +1,36 @@
+package fr.univavignon.pokedex.api;
+
+import java.util.Random;
+
+public class PokemonFactory implements IPokemonFactory {
+
+    private PokemonMetadataProvider pokemonMetadataProvider;
+
+    @Override
+    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+        try {
+            PokemonMetadata metadata = pokemonMetadataProvider.getPokemonMetadata(index);
+
+            Random randomGenerator = new Random();
+            int attack_iv = randomGenerator.nextInt(16);
+            int defense_iv = randomGenerator.nextInt(16);
+            int stamina_iv = randomGenerator.nextInt(16);
+            double iv = (attack_iv + defense_iv + stamina_iv)*45/100;
+
+            return new Pokemon(
+                index, 
+                metadata.getName(), 
+                metadata.getAttack() + attack_iv, 
+                metadata.getDefense() + defense_iv,
+                metadata.getStamina() + stamina_iv, 
+                cp, 
+                hp, 
+                dust, 
+                candy, 
+                iv);
+        } catch (PokedexException e) {
+            throw new ArrayIndexOutOfBoundsException(
+                "Pokemon not found with index " + String.valueOf(index));
+        }
+    }
+}
